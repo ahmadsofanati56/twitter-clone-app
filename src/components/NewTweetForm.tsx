400b2ff2c6837b169211d614041d44b8b5a1f9bc
ahmadsofanati56 <ahmadsofanati56@gmail.com>
2023-07-28 18:06:21 +0300

    add changes

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,49 +1,97 @@
 import { useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
-import { FormEvent, useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  FormEvent,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { api } from "~/utils/api";
 
-function updateTextAreaSize(textarea? : HTMLTextAreaElement){
-        if(textarea == null) return;
-        textarea.style.height ="0"
-        textarea.style.height= `${textarea.scrollHeight}px`
-        
-
+function updateTextAreaSize(textarea?: HTMLTextAreaElement) {
+  if (textarea == null) return;
+  textarea.style.height = "0";
+  textarea.style.height = `${textarea.scrollHeight}px`;
 }
-    function Form(){
-        const session = useSession()
-        const [inputValue,setInputValue] = useState("") ;
-     
-        const textAreaRef= useRef<HTMLTextAreaElement>();
-        const inputRef= useCallback((textarea: HTMLTextAreaElement)=>{
-            updateTextAreaSize(textarea);
-            textAreaRef.current=textarea;
-        },[]);
-        const createTweet=   api.tweet.create.useMutation({onSuccess : newTweet=>{console.log(newTweet);
-        setInputValue("")}});
-        useLayoutEffect(()=>{
-            updateTextAreaSize(textAreaRef.current)
-        },[inputValue])
-        function handleSubmit(e: FormEvent){
-            e.preventDefault()
+function Form() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const trpcUtils = api.useContext();
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textarea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textarea);
+    textAreaRef.current = textarea;
+  }, []);
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      setInputValue("");
 
-            createTweet.mutate({content: inputValue})
-        }
-        if(session.status !== "authenticated") return null;
-        return <form className="flex flex-col gap-3 border-b px-4 py-2" onSubmit={handleSubmit}>
-            <div className="flex gap-4">
-                <ProfileImage src={session.data?.user.image}/>
-                <textarea placeholder="What's happening?" ref={inputRef} style={{height : 0}}  className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" value={inputValue} onChange={e=>{setInputValue(e.target.value)}}></textarea>
-              
-            </div>
-            <Button className="self-end">Tweet</Button>
-        </form>
-    }
- export function NewTweetForm (){
-    const session = useSession()
-    if(session.status !== "authenticated") return;
+      if (session.status !== "authenticated") return;
 
-    return <Form/>
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return;
 
- }
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
+    },
+  });
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    createTweet.mutate({ content: inputValue });
+  }
+  if (session.status !== "authenticated") return null;
+  return (
+    <form
+      className="flex flex-col gap-3 border-b px-4 py-2"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex gap-4">
+        <ProfileImage src={session.data?.user.image} />
+        <textarea
+          placeholder="What's happening?"
+          ref={inputRef}
+          style={{ height: 0 }}
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        ></textarea>
+      </div>
+      <Button className="self-end">Tweet</Button>
+    </form>
+  );
+}
+export function NewTweetForm() {
+  const session = useSession();
+  if (session.status !== "authenticated") return;
+
+  return <Form />;
+}
--- src/components/sideNav.tsx
+++ src/components/sideNav.tsx
@@ -1,22 +1,70 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { IconHoverEffect } from "./IconHoverEffect";
+import { VscAccount, VscHome, VscSignIn, VscSignOut } from "react-icons/vsc";
 
-export function SideNav (){
-    const session = useSession();
-    const user= session.data?.user;
-return <nav className="sticky top-0 px-3 py-4">
-    <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
+export function SideNav() {
+  const session = useSession();
+  const user = session.data?.user;
+  return (
+    <nav className="sticky top-0 px-3 py-4">
+      <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
         <li>
-            <Link href={"/"}>Home</Link>
+          <Link href={"/"}>
+            <IconHoverEffect>
+              <span className="flex items-center gap-4">
+                <VscHome className="h-7 w-7" />
+
+                <span className=" hidden text-lg md:inline">Home</span>
+              </span>
+            </IconHoverEffect>
+          </Link>
         </li>
-        { user != null && <li>
-            <Link href={`/profiles/${user.id}`}>Profile</Link>
-        </li>}
-        {user == null ? (<li>
-            <button onClick={()=>void signIn()}>Log In</button>
-        </li>) :(<li>
-            <button onClick={()=>void signOut()}>Log Out </button>
-        </li>)}
-    </ul>
-</nav>
+        {user != null && (
+          <li>
+            <Link href={`/profiles/${user.id}`}>
+              {" "}
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscAccount className="h-7 w-7" />
+
+                  <span className=" hidden text-lg md:inline">Profile</span>
+                </span>
+              </IconHoverEffect>
+            </Link>
+          </li>
+        )}
+        {user == null ? (
+          <li>
+            <button onClick={() => void signIn()}>
+              {" "}
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscSignIn className="h-7 w-7 fill-green-700" />
+
+                  <span className=" hidden text-lg text-green-700 md:inline">
+                    Log In
+                  </span>
+                </span>
+              </IconHoverEffect>
+            </button>
+          </li>
+        ) : (
+          <li>
+            <button onClick={() => void signOut()}>
+              <IconHoverEffect red>
+                <span className="flex items-center gap-4">
+                  <VscSignOut className="h-7 w-7 fill-red-700" />
+
+                  <span className=" hidden text-lg text-red-700 md:inline">
+                    Log Out
+                  </span>
+                </span>
+              </IconHoverEffect>{" "}
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
