3eb0f11c38469838539589bedfab0a0a0e340b38
ahmadsofanati56 <ahmadsofanati56@gmail.com>
2023-07-28 18:09:05 +0300

    add changes

---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,39 @@
+import { Prisma } from "@prisma/client";
+import { inferAsyncReturnType } from "@trpc/server";
+import { useSession } from "next-auth/react";
+import next from "next/types";
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+  createTRPCContext,
+} from "~/server/api/trpc";
+
+export const profileRouter = createTRPCRouter({
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input: { id }, ctx }) => {
+      const currentUserId = ctx.session?.user.id;
+      const profile = await ctx.prisma.user.findUnique({
+        where: { id },
+        select: {
+          _count: { select: { followers: true, follows: true, tweets: true } },
+          name: true,
+          image: true,
+          followers:
+            currentUserId == null
+              ? undefined
+              : { where: { id: currentUserId } },
+        },
+      });
+      if (profile == null) return;
+      return {
+        name: profile.name,
+        image: profile.image,
+        followersCount: profile._count.followers,
+        followsCount: profile._count.follows,
+        tweetsCount: profile._count.tweets,
+      };
+    }),
+});
